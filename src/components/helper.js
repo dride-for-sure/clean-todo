293fca444fe27c33059663234481939687943cbd
dennisjauernig <dennis@jauernig.email>
2020-12-30 11:49:50 +0100

    Move addEventListenerMulti to helper.js

--- src/components/helper.js
+++ src/components/helper.js
@@ -6,3 +6,18 @@
 export const setTimeoutFunction = (callback, delay) => {
     setTimeout(callback, delay);
 }
+
+/**
+ * EventListener for multiple events
+ * @param {querySelectorAll} elements
+ * @param {Array} eventNames 
+ * @param {Function} listener 
+ */
+
+export const addEventListenerMulti = (elements, eventNames, listener) => {
+    document.querySelectorAll(elements).forEach(el => {
+        eventNames.forEach(event => {
+            el.addEventListener(event, listener, false);
+        })
+    })
+}
