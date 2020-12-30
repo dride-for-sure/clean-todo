a116afb1e79d61ae9b7b29318dc24e1772253263
dennisjauernig <dennis@jauernig.email>
2020-12-30 17:48:47 +0100

    Fix label visible on selection

--- src/components/ui.js
+++ src/components/ui.js
@@ -12,21 +12,13 @@ export const outputHTML = (e, a) => {
 		});
 	});
 
-	// 
+	// Toggle Invisible Class if before & after selection/caret are empty
+	toggleInvisibleClass(e, a, outputWithTags);
+
 	const html =
 		outputWithTags[0].join(' ') +
 		outputWithTags[1].join(' ') +
 		outputWithTags[2].join(' ');
-
-	// Toggle Empty Class if before & after selection/caret are empty
-	if (outputWithTags[0][0] === '' && outputWithTags[2][0] === '') {
-		// toggleEmptyClass(e, true);
-		helper.modifyClassList(e.target.parentNode.nextElementSibling, false ,'visible', true);
-
-	} else {
-		// toggleEmptyClass(e, false);
-		helper.modifyClassList(e.target.parentNode.nextElementSibling, false ,'visible', false);
-	}
 
 	e.target.nextElementSibling.innerHTML = html;
 };
@@ -46,3 +38,28 @@ export const insertHTMLTags = (o) => {
 		return `<span class="types type-${o.type}">${o.string}</span>`;
 	}
 };
+
+// TODO: REMOVE and check empty of input field
+/**
+ * Toggle invisible class on label when input is empty
+ * @param {Event} e 
+ * @param {Array} a 
+ * @param {Array} outputWithTags 
+ */
+const toggleInvisibleClass = (e, a, outputWithTags) => {
+	if (outputWithTags[0][0] === '' && a[1][0].type === 'cursor' && outputWithTags[2][0] === '') {
+		helper.modifyClassList(
+			e.target.parentNode.nextElementSibling,
+			false,
+			'invisible',
+			true
+		);
+	} else {
+		helper.modifyClassList(
+			e.target.parentNode.nextElementSibling,
+			false,
+			'invisible',
+			false
+		);
+	}
+}
