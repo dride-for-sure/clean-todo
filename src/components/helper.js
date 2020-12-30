dca0d605aa0472455c2aff8c5dfc943eb506bcde
dennisjauernig <dennis@jauernig.email>
2020-12-30 11:44:02 +0100

    Helper.js

---
+++ src/components/helper.js
@@ -0,0 +1,8 @@
+/**
+ * Delay function call
+ * @param {Function} callback
+ * @param {number} delay - ms
+ */
+export const setTimeoutFunction = (callback, delay) => {
+    setTimeout(callback, delay);
+}
