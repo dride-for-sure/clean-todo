287732a31d91c10cc9315996ed00d3b1cf0c30e1
dennisjauernig <dennis@jauernig.email>
2021-01-01 23:02:34 +0100

    Remove JSON Handling

--- src/components/cmd.js
+++ src/components/cmd.js
@@ -70,24 +70,3 @@ const getSelection = (e, s) => {
 
 	return output;
 };
-
-/**
- * Check if data-uuid is available, else create uuidv4
- * @param {Event} e
- * @returns {string} uuidv4
- */
-const uuidv4 = (e) => {
-	const uuid = e.target.getAttribute('data-uuid');
-	if (typeof uuid === true) {
-		return uuid;
-	} else {
-		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
-			/[xy]/g,
-			function (c) {
-				var r = (Math.random() * 16) | 0,
-					v = c == 'x' ? r : (r & 0x3) | 0x8;
-				return v.toString(16);
-			}
-		);
-	}
-};
