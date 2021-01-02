ca5ca10d3f5014173758c80a29b1ea61e0440308
dennisjauernig <dennis@jauernig.email>
2021-01-02 12:34:11 +0100

    Refactor ItemClass with Getter for HTML

--- src/components/item.js
+++ src/components/item.js
@@ -1,28 +1,39 @@
 export class Item {
-    constructor(id, desc, due = null, assign = null, priority = false, status = true) {
-        this.id = id;
+	constructor(
+		id,
+        desc,
+        tags = '',
+		due = null,
+		assign = null,
+		prio = false,
+		status = true
+	) {
+		this.id = id;
         this.desc = desc;
-        this.due = due;
-        this.assign = assign;
-        this.priority = priority;
-        this.status = status;
-    }
+        this.tags = tags;
+		this.due = due;
+		this.assign = assign;
+		this.prio = prio;
+		this.status = status;
+	}
 
-    get html() {
-        const html = `
+	get html() {
+		return `
             <div class="col">
                 <div class="position-relative">
                     <div class="m-0 cmd cmd-secondary due">
                         <input class="border-0" type="text"
-                            data-task-id="${obj.id}"
-                            data-task-desc="${obj.desc}"
-                            data-task-due="${obj.due}"
-                            data-task-assign="${obj.assign}"
-                            data-task-priority="${obj.priority}"
+                            data-task-id="${this.id}"
+                            data-task-desc="${this.desc}"
+                            data-task-tags="${this.tags}"
+                            data-task-due="${this.due}"
+                            data-task-assign="${this.assign}"
+                            data-task-prio="${this.prio}"
+                            data-task-status="${this.status}"
                             >
                         <p></p>
                     </div>
                 </div>               
-            </div>`
-    }
+            </div>`;
+	}
 }
