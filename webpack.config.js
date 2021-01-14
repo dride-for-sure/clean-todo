bc22a66eb65235658ed9928a9e2d8a691f940b6b
dennisjauernig <dennis@jauernig.email>
2021-01-14 16:10:31 +0100

    Add file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 // Make path available
-const path = require('path');
+const path = require('path')
 
 // Setup directories
-const srcDir = path.resolve(__dirname, './src');
-const distDir = path.resolve(__dirname, './dist');
+const srcDir = path.resolve(__dirname, './src')
+const distDir = path.resolve(__dirname, './dist')
 
 // Detect production and development mode
-const isDevelopment = process.env.NODE_ENV === 'development';
+const isDevelopment = process.env.NODE_ENV === 'development'
 
 // Html Loader
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 // Cleanup dist folder
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 // Extract css into separate files
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 // Config object to export
 module.exports = {
@@ -23,14 +23,14 @@ module.exports = {
   entry: `${srcDir}/js/index.js`,
   output: {
     path: distDir,
-    filename: isDevelopment ? 'bundle.js' : 'bundle.[contenthash].js', // https://github.com/webpack/loader-utils#interpolatename
+    filename: isDevelopment ? 'bundle.js' : 'bundle.[contenthash].js' // https://github.com/webpack/loader-utils#interpolatename
     // publicPath: '/', // ?
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.scss'],
+    extensions: ['.js', '.jsx', '.scss']
   },
   optimization: {
-    minimize: !isDevelopment, // No need for uglifyjs here at the moment, later?
+    minimize: !isDevelopment // No need for uglifyjs here at the moment, later?
   },
   module: {
     rules: [
@@ -41,10 +41,19 @@ module.exports = {
           {
             loader: 'html-loader',
             options: {
-              minimize: !isDevelopment,
-            },
-          },
-        ],
+              minimize: !isDevelopment
+            }
+          }
+        ]
+      },
+
+      // File-Loader
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'assets'
+        }
       },
 
       // Babel
@@ -54,9 +63,9 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+            presets: ['@babel/preset-env']
+          }
+        }
       },
 
       // SCSS/SASS Modules
@@ -70,28 +79,26 @@ module.exports = {
             loader: 'css-loader',
             options: {
               modules: true,
-              sourceMap: isDevelopment,
-            },
+              sourceMap: isDevelopment
+            }
           },
           {
             // Loader for webpack to process CSS with PostCSS
             loader: 'postcss-loader',
             options: {
               postcssOptions: {
-                plugins: [
-                  ['autoprefixer'],
-                ],
-              },
-            },
+                plugins: [['autoprefixer']]
+              }
+            }
           },
           {
             // Loads a SASS/SCSS file and compiles it to CSS
             loader: 'sass-loader',
             options: {
-              sourceMap: isDevelopment,
-            },
-          },
-        ],
+              sourceMap: isDevelopment
+            }
+          }
+        ]
       },
 
       // SCSS/SASS
@@ -108,22 +115,20 @@ module.exports = {
             loader: 'postcss-loader',
             options: {
               postcssOptions: {
-                plugins: [
-                  ['autoprefixer'],
-                ],
-              },
-            },
+                plugins: [['autoprefixer']]
+              }
+            }
           },
           {
             // Loads a SASS/SCSS file and compiles it to CSS
             loader: 'sass-loader',
             options: {
-              sourceMap: isDevelopment,
-            },
-          },
-        ],
-      },
-    ],
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      }
+    ]
   },
 
   // Init plugins
@@ -131,17 +136,17 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebPackPlugin({
       template: './src/index.html',
-      filename: './index.html',
+      filename: './index.html'
     }),
     new MiniCssExtractPlugin({
       filename: isDevelopment ? '[name].css' : '[name].[contenthash].css',
-      chunkFilename: isDevelopment ? '[id].css' : '[id].[contenthash].css',
-    }),
+      chunkFilename: isDevelopment ? '[id].css' : '[id].[contenthash].css'
+    })
   ],
 
   // DevServer settings
   devServer: {
-    contentBase: distDir,
+    contentBase: distDir
   },
-  devtool: isDevelopment ? 'inline-source-map' : 'none',
-};
+  devtool: isDevelopment ? 'inline-source-map' : 'none'
+}
