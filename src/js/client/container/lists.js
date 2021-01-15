3d5e1848867badb138c7d2e45dc108488c85bb80
dennisjauernig <dennis@jauernig.email>
2021-01-15 18:00:42 +0100

    Rename listview -> lists

--- src/js/client/container/lists.js
+++ src/js/client/container/lists.js
@@ -3,10 +3,15 @@ import getHeader from '../components/header/header';
 import getListsPredefined from '../components/lists/listsPredefined';
 import getListsUserDefined from '../components/lists/listsUserDefined';
 
-export default function listView(view, title) {
+/**
+ * Returns lists view
+ * @param {Object} data
+ * @param {String} view
+ */
+export default function lists(data, view) {
   return `
   <div class="container" tabindex="0">
-    ${getHeader(view, title)}
+    ${getHeader(view)}
     ${getListsUserDefined()}
   </div>
   <div class="container fixed-bottom">
