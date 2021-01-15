c63cd7704c684214222197923f9a3a6c58a5ef1a
dennisjauernig <dennis@jauernig.email>
2021-01-15 23:34:54 +0100

    Delete old getList, getNotos

--- src/js/client/components/header/getList.js
+++
@@ -1,9 +0,0 @@
-/**
- * Return a list object
- * @param {Object} data
- * @param {String} id
- * @returns {Object}
- */
-export default function getList(data, id) {
-  return data.lists.find(list => list.id === id);
-}
