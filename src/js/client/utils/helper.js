4c0f77f4f48066accdf9025dfe493464167f790e
dennisjauernig <dennis@jauernig.email>
2021-01-15 11:06:01 +0100

    Update helper fct with prettier rules

--- src/js/client/utils/helper.js
+++ src/js/client/utils/helper.js
@@ -3,7 +3,7 @@
  * @param {String} selector - class or id
  * @returns {NodeList}
  */
-export const getElements = (selector) => document.querySelectorAll(selector);
+export const getElements = selector => document.querySelectorAll(selector);
 
 /**
  * Create Element with/without class or id
@@ -14,7 +14,7 @@ export const getElements = (selector) => document.querySelectorAll(selector);
 export const createElement = (tag, selectors) => {
   const element = document.createElement(tag);
   if (selectors) {
-    selectors.split(' ').forEach((selector) => {
+    selectors.split(' ').forEach(selector => {
       element.classList.add(selector);
     });
   }
@@ -25,13 +25,11 @@ export const createElement = (tag, selectors) => {
  * UUIDv4
  * @returns {string}
  */
-export const uuid = () => 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
-  /[xy]/g,
-  (c) => {
+export const uuidv4 = () =>
+  'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
     // eslint-disable-next-line no-bitwise
     const r = (Math.random() * 16) | 0;
     // eslint-disable-next-line no-bitwise, eqeqeq
     const v = c == 'x' ? r : (r & 0x3) | 0x8;
     return v.toString(16);
-  },
-);
+  });
