90857b06a045b10a7a4cc2bace1e7e43e4009fe7
dennisjauernig <dennis@jauernig.email>
2021-01-15 10:29:53 +0100

    Add due to task class

--- src/js/client/model/task.js
+++ src/js/client/model/task.js
@@ -2,11 +2,16 @@
  * @class Task
  *
  * A single task
+ * @param {string} id - uuidv4
+ * @param {string} text
+ * @param {string} due - date
+ * @param {boolean} complete
  */
 export default class Task {
-  constructor(id, text, complete = false) {
+  constructor(id, text, due = undefined, complete = false) {
     this.id = id;
     this.text = text;
+    this.due = due;
     this.complete = complete;
   }
 }
